a297f24180f334114c68e23481d87fa22f79e9bd
S-Rohini <84655753+S-Rohini@users.noreply.github.com>
2022-04-09 07:29:36 -0700

    Add files via upload

---
+++ pratical/ArrayExample.java
@@ -0,0 +1,10 @@
+class ArrayExample{
+public static void main(String args[]){
+
+String [] bts={"V", "JK", "RM", "JIN", "JIMIN", "JHOPE", "SUGA"};
+//System.out.println(bts[5]);
+for (int i=0;i<=bts.length;i++){
+System.out.println(bts[i]);
+}
+}
+}
