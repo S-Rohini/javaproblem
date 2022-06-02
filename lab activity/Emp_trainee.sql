a2fd54afa2d9b0902dfe496181e132d3225a81a7
S-Rohini <84655753+S-Rohini@users.noreply.github.com>
2022-06-02 21:18:42 +0530

    Add files via upload

---
+++ lab activity/Emp_trainee.sql
@@ -0,0 +1,34 @@
+create database p1236;
+use p1236;
+
+create table Emp_trainee (
+eid numeric(2),
+ename varchar(10),
+ebasic numeric(6),
+designation varchar(10),
+eage numeric(2)
+);
+
+#rename
+alter table Emp1 CHANGE COLUMN eid Emp_trainee int;
+
+alter table Emp1 CHANGE COLUMN eage Age_in_Years int;
+
+#insert
+insert into Emp_trainee values
+(1,'Rohit',6700,'Trainee',24),
+(2,'Sunil',6200,'Trainee',27),
+(3,'Payal',6300,'Trainee',25),
+(4,'Kunal',6700,'Trainee',28),
+(5,'Sunita',6230,'Trainee',26),
+(6,'Bimal',7000,'Trainee',25); 
+
+#drop col
+alter table Emp_trainee DROP COLUMN eage;
+ 
+
+
+
+
+
+
