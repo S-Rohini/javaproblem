20515a5acea663dc35e1bae0d2e7ff4589eb3a59
S-Rohini <84655753+S-Rohini@users.noreply.github.com>
2022-07-14 20:14:46 +0530

    Add files via upload

---
+++ lab activity/VaccineElagegbilityException.java
@@ -0,0 +1,11 @@
+package com;
+
+@SuppressWarnings("serial")
+public class VaccineElagegbilityException extends Exception {
+
+
+	public VaccineElagegbilityException(String str) {
+		super(str);
+	}
+
+}
